01cce89abcef6f939c6456d17e2602664429103e
nytro77 <32847734+nytro77@users.noreply.github.com>
2023-03-02 23:43:41 +0100

    Removed connection pool

--- src/test/java/nytro77/calcitetimestampbug/ApplicationTests.java
+++ src/test/java/nytro77/calcitetimestampbug/ApplicationTests.java
@@ -4,6 +4,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
@@ -13,11 +19,8 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-
-import com.zaxxer.hikari.HikariDataSource;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
@@ -45,21 +48,25 @@ class ApplicationTests {
 	}
 
 	@Test
-	public void testTimestamps() {
+	void testTimestamps() throws SQLException {
 
-		try (HikariDataSource ds = new HikariDataSource()) {
-			ds.setMaximumPoolSize(1);
-			ds.setJdbcUrl(
-					"jdbc:avatica:remote:url=http://localhost:" + avaticaServer.getPort() + ";serialization=protobuf;");
+		List<String> result = new ArrayList<>();
 
-			JdbcTemplate jdbcTemplate = new JdbcTemplate(ds);
+		try (Connection connection = DriverManager.getConnection(
+				"jdbc:avatica:remote:url=http://localhost:" + avaticaServer.getPort() + ";serialization=protobuf;");
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("select THETIMESTAMP from CSV.TSTAMPS")) {
 
-			List<String> result = jdbcTemplate.queryForList("select THETIMESTAMP from CSV.TSTAMPS", String.class);
-
-			assertEquals(101, result.size());
-			assertEquals("1900-01-01 00:00:00", result.get(0));
-			assertEquals("1900-01-01 00:00:00", result.get(99));
-			assertEquals("1900-01-01 00:00:00", result.get(100), "Timestamp 101 is incorrect!"); // Bug will cause this to be 1899-12-31 23:00:00
+			while (resultSet.next()) {
+				result.add(resultSet.getString(1));
+			}
 		}
+
+		assertEquals(101, result.size());
+		assertEquals("1900-01-01 00:00:00", result.get(0));
+		assertEquals("1900-01-01 00:00:00", result.get(99));
+		assertEquals("1900-01-01 00:00:00", result.get(100), "Timestamp 101 is incorrect!"); // Bug will cause this to
+																								// be 1899-12-31
+																								// 23:00:00
 	}
 }
